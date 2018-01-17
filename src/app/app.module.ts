10439b289da037035d1d5bee11bca00e66cacbb9
sam <osenseijedi@gmail.com>
2018-01-17 20:40:21 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {MarkdownModule} from 'angular2-markdown';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MkdComponent} from './mkd/mkd.component';
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MkdComponent,
+    ],
+    imports: [
+        AppRoutingModule,
+        BrowserModule,
+        HttpClientModule,
+        MarkdownModule.forRoot(),
+    ],
+    providers: [HttpClientModule,],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
